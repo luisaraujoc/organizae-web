4807f071d9676724295a46657a12e8c9ed576e85
Enzzo Hosaki <39446235+EnzzoHosaki@users.noreply.github.com>
2025-03-07 09:41:27 -0300

    instalando o Tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
